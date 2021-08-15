c33a48da7dbbcd87be69ff75e70f963c3fbd510f
ameybhojane <88000728+ameybhojane@users.noreply.github.com>
2021-08-15 23:59:04 +0530

    update shape

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,16 @@
 // declare class
+// declare class
+class Shape{
+    constructor(color){
+    this.color=color;
+    }
+    drawShape(){
+    
+    }
+    calculateArea(){
 
+    } 
+}
+// export class using module.exports
+module.exports.Shape=Shape;
 // export class using module.exports
