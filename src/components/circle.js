ee6719f392e847d26edeb06a6e5d72272db62c33
ameybhojane <88000728+ameybhojane@users.noreply.github.com>
2021-08-15 23:58:21 +0530

    update

--- src/components/circle.js
+++ src/components/circle.js
@@ -2,5 +2,19 @@
 
 // declare class
 
+// import using require
+const {Shape} = require('./shape.js') 
+// const imp={require('./shape.js')}
 
+// declare class
+class Circle extends Shape{
+    constructor(color){
+        super(color);
+    }
+    calculateArea(){
+        return "pi*r*Square";
+    }
+}
+module.exports.Circle=Circle;
+// export class using module.exports
 // export class using module.exports
