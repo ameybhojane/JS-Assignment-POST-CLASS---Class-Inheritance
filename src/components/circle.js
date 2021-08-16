86bce6dd796b9d9382b5b062f592122bd77b9840
ameybhojane <88000728+ameybhojane@users.noreply.github.com>
2021-08-16 16:46:54 +0530

    new commit

--- src/components/circle.js
+++ src/components/circle.js
@@ -3,18 +3,18 @@
 // declare class
 
 // import using require
-const {Shape} = require('./shape.js') 
+var Shape = require('./shape.js') 
 // const imp={require('./shape.js')}
 
 // declare class
 class Circle extends Shape{
-    constructor(color){
-        super(color);
+    constructor(){
+        super();
     }
     calculateArea(){
         return "pi*r*Square";
     }
 }
-module.exports.Circle=Circle;
+module.exports=Circle;
 // export class using module.exports
 // export class using module.exports
