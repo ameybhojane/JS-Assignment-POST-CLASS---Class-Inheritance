654f60e6853961749996ec5be07d77106b43b9d3
ameybhojane <88000728+ameybhojane@users.noreply.github.com>
2021-08-16 16:45:34 +0530

    New commit

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,16 +1,16 @@
 // declare class
 // declare class
 class Shape{
-    constructor(color){
-    this.color=color;
+    constructor(){
+    this.color=blue;
     }
     drawShape(){
-    
+    return "Shape is drawn";
     }
     calculateArea(){
-
+    return "Area is Caluclated"
     } 
 }
 // export class using module.exports
-module.exports.Shape=Shape;
+module.exports=Shape;
 // export class using module.exports
